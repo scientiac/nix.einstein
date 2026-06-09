5c0f96f8a39cc3b15411ea5b9a1887d648630735
scientiac <iacsci@proton.me>
2026-06-09 23:38:21 +0545

    add!(zathura): use system clipboard for copy , and a fullscreen shortcut

--- modules/features/zathura/default.nix
+++ modules/features/zathura/default.nix
@@ -17,11 +17,18 @@
         options = {
           guioptions = 0;
           adjust-open = "best-fit";
+          selection-clipboard = "clipboard";
           font = "FantasqueSansM Nerd Font Mono 10";
+
+          scroll-page-aware = true;
+
+          window-title-basename = true;
+          window-title-page = true;
         };
 
         mappings = {
           f = "toggle_fullscreen";
+          "[fullscreen] f" = "toggle_fullscreen";
         };
       };
     };
