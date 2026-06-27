687200a09d74292958df02e760d70b5dab6246b5
scientiac <iacsci@proton.me>
2026-06-27 15:34:04 +0545

    fix!(fish): prompt char

--- modules/features/fish/default.nix
+++ modules/features/fish/default.nix
@@ -34,7 +34,7 @@
             set_color $fish_color_cwd
             echo -n (path basename $PWD)
             set_color --reset
-            echo -n ' ) '
+            echo -n ' 󰅂 '
           '';
         };
       };
