5f486bc25111ce6928186ab16ba7d73e76a83766
scientiac <iacsci@proton.me>
2026-08-16 19:43:09 +0545

    fix!(fonts): nepali/indic ligatures support

--- modules/hosts/einstein/configuration.nix
+++ modules/hosts/einstein/configuration.nix
@@ -49,43 +49,6 @@
         enableDefaultPackages = false;
       };
 
-      # fonts.fontconfig = {
-      #   enable = true;
-      #
-      #   localConf = ''
-      #     <?xml version="1.0"?>
-      #     <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-      #     <fontconfig>
-      #       <!-- Prefer Noto for Devanagari -->
-      #       <match target="pattern">
-      #         <test name="lang" compare="contains">
-      #           <string>ne</string>
-      #         </test>
-      #         <edit name="family" mode="prepend" binding="strong">
-      #           <string>Noto Sans Devanagari</string>
-      #           <string>Noto Serif Devanagari</string>
-      #         </edit>
-      #       </match>
-      #
-      #       <!-- Override generic sans -->
-      #       <alias>
-      #         <family>sans-serif</family>
-      #         <prefer>
-      #           <family>Noto Sans Devanagari</family>
-      #         </prefer>
-      #       </alias>
-      #
-      #       <!-- Override generic serif -->
-      #       <alias>
-      #         <family>serif</family>
-      #         <prefer>
-      #           <family>Noto Serif Devanagari</family>
-      #         </prefer>
-      #       </alias>
-      #     </fontconfig>
-      #   '';
-      # };
-
       # System Settings
       imports = [
         self.nixosModules.einsteinHardware
