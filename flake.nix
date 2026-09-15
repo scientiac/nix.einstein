2271a897b86319068b3f278db574f3c0a7dfe369
scientiac <iacsci@proton.me>
2026-09-15 23:36:25 +0545

    fix!(howdy): unlocks and polkit work but not keyring as it requires password

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/features/howdy/default.nix
+++ modules/features/howdy/default.nix
@@ -18,5 +18,24 @@
 
       services.howdy-next.enable = true;
       security.polkit.enable = true;
+
+      security.pam.services =
+        lib.genAttrs
+          [
+            "sudo"
+            "login"
+            "polkit-1"
+          ]
+          (name: {
+            rules.auth.howdy-next = {
+              control = "sufficient";
+              modulePath = "${config.services.howdy-next.package}/lib/security/pam_howdy.so";
+              args = if name == "polkit-1" then [ "workaround=native" ] else [ "workaround=native-input" ];
+              order =
+                config.security.pam.services.${name}.rules.auth.unix.order
+                + (if name == "polkit-1" then -100 else 100);
+            };
+          });
+
     };
 }
